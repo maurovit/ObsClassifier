0756a3c3ace4b654bfe4a464de64d20e3b1f3815
Mauro Vitale <vitalemauro@outlook.it>
2018-06-16 14:06:27 +0200

    Initial Version

---
+++ iris_data.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import tensorflow as tf
+
+TRAIN_PATH = "observer_dp_dataset.csv"
+TEST_PATH= "----"
+CSV_COLUMN_NAMES = ['CollectionVariables','AddListenerMethod','RemoveListenerMethod',
+                    'ClassDeclarationKeyword','MethodsDeclarationKeyword','ClassType','ScanCollectionsMethod',
+                    'SCMCallsAbsMethod','HasSuperclass','ImplementsInterfaces','ChangeState',
+                    'AfterChangeStateIterateOverList','NOPM','Role']
+ROLES = ['Subject', 'Observer', 'ConcreteSubject', 'ConcreteObserver']
+
+def load_data(y_name='Role'):
+    """Returns the iris dataset as (train_x, train_y), (test_x, test_y)."""
+    train = pd.read_csv(TRAIN_PATH, names=CSV_COLUMN_NAMES, header=0, delimiter=';')
+    train_x, train_y = train, train.pop(y_name)
+    #test = pd.read_csv(TEST_PATH, names=CSV_COLUMN_NAMES, header=0)
+    #test_x, test_y = test, test.pop(y_name)
+    return (train_x, train_y), (None,None) #Modificare
+
+def train_input_fn(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
+
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+    # Return the dataset.
+    return dataset
+
+def eval_input_fn(features, labels, batch_size):
+    """An input function for evaluation or prediction"""
+    features=dict(features)
+    if labels is None:
+        # No labels, use only features.
+        inputs = features
+    else:
+        inputs = (features, labels)
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices(inputs)
+    # Batch the examples
+    assert batch_size is not None, "batch_size must not be None"
+    dataset = dataset.batch(batch_size)
+    # Return the dataset.
+    return dataset
+
+def main():
+    (train_x, train_y), (test_x, test_y)=load_data();
+    print("Xs\n-----------------------------\n",train_x);
+    print("Ypsilons\n-----------------------------\n",train_y);
+    dataset=train_input_fn(train_x,train_y,10);
+    print("Dataset\n-----------------------------\n",dataset);
+    dataset.
+
+if __name__ == '__main__':
+    main();
